c00582f305ac2bb7047ea4f8c6ebc5ec22eff9c7
liuxiaoqingchn <liuxiaoqingchn@outlook.com>
2018-08-04 17:29:30 +0800

    提交密闭类静态类及扩展方法

---
+++ 如鹏课堂练习/提高班/Net高级技术/密闭类静态类及扩展方法/MyEx/StringEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEx
+{
+    //扩展方法所在的类必须是static类
+    //扩展方法的第一个参数类型是被扩展的类型，类型前面标注为this
+    public static class StringEx
+    {
+        public static bool IsEmail(this string s)
+        {
+            return s.Contains("@");
+        }
+        
+        public static string Test(this SqlConnection connection, string sql)
+        {
+            return "执行成功";
+        }
+
+    }
+}
---
+++ 如鹏课堂练习/提高班/Net高级技术/密闭类静态类及扩展方法/密闭类静态类及扩展方法/Program.cs
@@ -0,0 +1,97 @@
+﻿using MyEx;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 密闭类静态类及扩展方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string e = "aaa.@qq.com";
+            //bool b = Person.IsEmail(e);
+
+            //Console.WriteLine(e.IsEmail());
+
+            //string s = "abc";
+            //string s1 = Person.Repeat(s, 3);
+            //Console.WriteLine(s.Repeat(5));
+            //Console.WriteLine(s1);
+            //Consoles.WriteLine(b);
+
+            string a = "aaa";
+            Console.WriteLine(a.IsEmail());
+
+            SqlConnection conn = new SqlConnection();
+            string b= conn.Test("hhh");
+
+
+            Console.WriteLine(b);
+            
+            Console.ReadKey();
+        }
+    }
+
+    /*
+    sealed class Dog
+    {
+
+    }
+
+    class Bird : Dog
+    {
+
+    }
+
+    class A : String
+    {
+
+    }
+    
+    static class Person
+    {
+        public int Age { get; set; }
+
+        public int age;
+
+        public static string Gender;
+
+        public static string Gender { get; set; }
+
+        public void SayHi()
+        {
+
+        }
+
+        public static bool IsEmail()
+        {
+            return false;
+        }
+    }
+    */
+
+    static class Person
+    {
+        ////public static bool IsEmail(this string s)
+        //{
+        //    return s.Contains("@");
+        //}
+
+        public static string Repeat(this string s, int count)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                result += s;
+            }
+            return result;
+        }
+
+
+    }
+   
+}
