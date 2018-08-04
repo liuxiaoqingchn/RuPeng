b046eb7734eb12b0534cda84e3cef67aa2a30a51
liuxiaoqingchn <liuxiaoqingchn@outlook.com>
2018-08-04 17:34:18 +0800

    提交HTTPClient

---
+++ 如鹏课堂练习/提高班/Net并发编程/HttpClient/Test1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient hc = new HttpClient();
+            string html = await hc.GetStringAsync("http://www.github.com");
+            textBox1.Text = html;
+            MessageBox.Show(html);
+        }
+    }
+}
