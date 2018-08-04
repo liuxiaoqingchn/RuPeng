e144b1a1204b8c84c61091245f37f16e610a0af9
liuxiaoqingchn <liuxiaoqingchn@outlook.com>
2018-08-04 17:30:39 +0800

    提交索引器

---
+++ 如鹏课堂练习/提高班/Net高级技术/索引器/索引器/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 索引器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jake";
+            //char c = name[1];
+
+            Person p = new Person();
+            p[3, 5] = "Hello";
+            string s = p[1,2];
+
+            Dog d = new Dog();
+            d["hello"] = 5;
+            int i = d["yeye"];
+
+            Console.WriteLine(i);
+            //Console.WriteLine(s);
+            Console.ReadKey();
+        }
+
+        class Person
+        {
+            public string this[int x, int y]
+            {
+                get
+                {
+                    return "" + x + y;
+                }
+                set
+                {
+                    Console.WriteLine("x="+x+"y="+y);
+                }
+            }
+        }
+
+        class Dog
+        {
+            public int this[string s]
+            {
+                get
+                {
+                    return 888;
+                }
+
+                set
+                {
+                    Console.WriteLine("s="+s);
+                }
+            }
+        }
+
+    }
+}
